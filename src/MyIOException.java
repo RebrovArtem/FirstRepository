96904da2e9329c699980934e32dd73dc564abc7d
Artem Rebrov <Tvitty2009@mail.ru>
2025-10-24 21:57:37 +0300

    "Forth change"

--- src/MyIOException.java
+++ src/MyIOException.java
@@ -1,6 +1,9 @@
-import java.io.IOException;
+import java.io.Serial;
 
 public class MyIOException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = -4485933467358737598L;
+
     public MyIOException(){
         super("Ошибка при работе с файлом");
     }
--- src/Person.java
+++ src/Person.java
@@ -4,20 +4,12 @@ import java.io.Serializable;
 public class Person implements Serializable {
     @Serial
     private static final long serialVersionUID = 2850941593282398964L;
-    private transient int id;
-    private String name;
+    private final transient int id;
+    private final String name;
 
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     @Override
